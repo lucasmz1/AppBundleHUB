3cbd5785dbb867c6ed0a497ea06af008c3a4552a
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2024-11-25 20:01:10 -0300

    Update libreoffice.sh

--- recipes/generic/libreoffice.sh
+++ recipes/generic/libreoffice.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "lucasmz1" -n "libreoffice" -p "libreoffice" -e "libreoffice7.6.7.2-startcenter.desktop"
+pelfCreator -m "lucasmz1" -n "libreoffice" -p "libreoffice" -e "libreoffice*-startcenter.desktop"
