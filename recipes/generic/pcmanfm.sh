f9a4b93cf04f46bff4c82ce5fcc27dafd91b8dd7
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2024-11-29 20:33:32 -0300

    Update pcmanfm.sh

--- recipes/generic/pcmanfm.sh
+++ recipes/generic/pcmanfm.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "pcmanfm" -p "pcmanfm" -x "usr/bin/pcmanfm" -e "pcmanfm.desktop"
+arch_armv7 pelfCreator -m "lucasmz1" -n "pcmanfm" -p "pcmanfm" -x "usr/bin/pcmanfm" -e "pcmanfm.desktop"
