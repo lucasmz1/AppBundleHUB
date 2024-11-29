6def8227ff0019c1bd6cbb873be9f1058348d30a
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2024-11-29 19:57:43 -0300

    Update libreoffice.sh

--- recipes/generic/libreoffice.sh
+++ recipes/generic/libreoffice.sh
@@ -1,1 +1,1 @@
-AUR=1 arch_armv7 pelfCreator -m "lucasmz1" -n "libreoffice" -p "libreoffice" -e "libreoffice7.6"
+arch_armv7 pelfCreator -m "lucasmz1" -n "libreoffice" -p "libreoffice" -e "libreoffice7.6"
