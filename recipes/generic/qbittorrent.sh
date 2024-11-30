4b43c6d0473302a365b197c589a6fc0e3aaf460d
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2024-11-30 17:53:00 -0300

    Update qbittorrent.sh

--- recipes/generic/qbittorrent.sh
+++ recipes/generic/qbittorrent.sh
@@ -1,1 +1,1 @@
-arch_armv7 pelfCreator -m "xplshn" -n "org.qbittorrent.qBittorrent" -p "qbittorrent" -e "org.qbittorrent.qBittorrent.desktop"
+arch_armv7 pelfCreator -m "xplshn" -n "org.qbittorrent.qBittorrent" -p "qbittorrent" -e "org.qbittorrent.qBittorrent.desktop" --dontpack
