957b0376ed33b62402e20420cc8011f4bb0078ef
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2024-12-01 11:52:56 -0300

    Update thunar.sh

--- recipes/generic/thunar.sh
+++ recipes/generic/thunar.sh
@@ -1,1 +1,1 @@
-AUR=1 arch_armv7 pelfCreator -m "xplshn" -n "thunar" -p "thunar" -x "usr/bin/thunar usr/bin/thunar-settings" -e "thunar.desktop"
+AUR=1 arch_armv7 pelfCreator -m "xplshn" -n "thunar" -p "thunar" -x "usr/bin/thunar usr/bin/thunar-settings" -e "thunar.desktop" --dontpack
