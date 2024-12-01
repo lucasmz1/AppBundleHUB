fe3fe1af91130192214189fe45b99901f6448e6b
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2024-12-01 17:49:08 -0300

    Update libreoffice.sh

--- recipes/generic/libreoffice.sh
+++ recipes/generic/libreoffice.sh
@@ -1,1 +1,1 @@
-PATTERN=arch_armv7 pelfCreator --maintainer lucasmz1 --name libreoffice --pkg-add libreoffice --entrypoint libreoffice --dontpack
+ARCH=armv7 pelfCreator --maintainer lucasmz1 --name libreoffice --pkg-add libreoffice --entrypoint libreoffice --dontpack
