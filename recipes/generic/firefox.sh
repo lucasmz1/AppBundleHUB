570792cc1f964839bdfc27d86938375d301de7a5
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2024-12-01 11:43:16 -0300

    Update firefox.sh

--- recipes/generic/firefox.sh
+++ recipes/generic/firefox.sh
@@ -1,1 +1,1 @@
-arch_armv7 pelfCreator -m "xplshn" -n "org.mozilla.firefox" -p "firefox" -e "firefox.desktop"
+armv7 pelfCreator -m "lucasmz1" -n "org.mozilla.firefox" -p "firefox" -e "firefox.desktop"
